d94aef6f685ef486b59e8429c4dac997a0b4c151
Aglorios17 <alessio.glorioso17@gmail.com>
2025-05-20 16:54:10 +0200

    Refactor fundingScheme property to use array type for improved schema consistency and update form fields to reflect changes in funding structure

--- src/app/types/TaxonomicService.d.ts
+++ src/app/types/TaxonomicService.d.ts
@@ -366,42 +366,7 @@ export interface TaxonomicService {
   /**
    * A FundingScheme combines organizational, project and policy aspects of grant-based funding that sets guidelines, principles and mechanisms to support other kinds of projects and activities.
    */
-  "schema:fundingScheme"?: {
-    /**
-     * An award won by or for this service
-     */
-    "schema:award"?: string;
-    /**
-     * A Grant that directly or indirectly provide funding or sponsorship for this service
-     */
-    "schema:funding"?: {
-      /**
-       * A unique identifier to identify the grant
-       */
-      "schema:identifier"?: string;
-      /**
-       * A description of the service's grant
-       */
-      "schema:description"?: string;
-    };
-    /**
-     * An organization that supports (sponsors) something through some kind of financial contribution.
-     */
-    "schema:funder"?: {
-      /**
-       * The type of funder
-       */
-      "@type": "schema:Organization";
-      /**
-       * A unique identifier to identify the funder organisation; ROR identifiers are valid
-       */
-      "schema:identifier": string;
-      /**
-       * The name of the funder (funding program)
-       */
-      "schema:name"?: string;
-    }[];
-  };
+  "schema:fundingScheme"?: unknown[];
   /**
    * Object representing the service's software source code
    */
