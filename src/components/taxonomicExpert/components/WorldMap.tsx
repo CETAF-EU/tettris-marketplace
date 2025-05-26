ae71d15493e17f9a6356af547fa8b33375293e64
Aglorios17 <alessio.glorioso17@gmail.com>
2025-05-26 19:55:49 +0200

    Update TaxonomicExpertForm and TaxonomicExpertFilters with detailed expertise options
    - Enhanced the expertise options in TaxonomicExpertForm.json by adding descriptive labels for each keyword, improving clarity for users.
    - Updated TaxonomicExpertFilters.json to reflect the new detailed labels for the expertise options, ensuring consistency across the application.
    
    - Issue
    React Map not working with non contries values for now

--- src/components/taxonomicExpert/components/WorldMap.tsx
+++ src/components/taxonomicExpert/components/WorldMap.tsx
@@ -5,234 +5,76 @@ import {
 } from 'react-simple-maps';
 
 // URL to the TopoJSON world map
-const geoUrl = "https://unpkg.com/world-atlas@2.0.2/countries-110m.json";
+const geoUrl = "https://unpkg.com/world-atlas@2.0.2/countries-50m.json";
 
 const europeanCountries = [
-  "Albania",
-  "Andorra",
-  "Austria",
-  "Belarus",
-  "Belgium",
-  "Bosnia and Herzegovina",
-  "Bulgaria",
-  "Croatia",
-  "Cyprus",
-  "Czech Republic",
-  "Denmark",
-  "Estonia",
-  "Finland",
-  "France",
-  "Germany",
-  "Greece",
-  "Hungary",
-  "Iceland",
-  "Ireland",
-  "Italy",
-  "Kosovo",
-  "Latvia",
-  "Liechtenstein",
-  "Lithuania",
-  "Luxembourg",
-  "Malta",
-  "Moldova",
-  "Monaco",
-  "Montenegro",
-  "Netherlands",
-  "North Macedonia",
-  "Norway",
-  "Poland",
-  "Portugal",
-  "Romania",
-  "San Marino",
-  "Serbia",
-  "Slovakia",
-  "Slovenia",
-  "Spain",
-  "Sweden",
-  "Switzerland",
-  "Ukraine",
-  "United Kingdom",
-  "Vatican City",
+  "Albania", "Andorra", "Austria", "Belarus", "Belgium", "Bosnia and Herz.",
+  "Bulgaria", "Croatia", "Cyprus", "Czechia", "Denmark", "Estonia", "Finland",
+  "France", "Germany", "Greece", "Hungary", "Iceland", "Ireland", "Italy",
+  "Kosovo", "Latvia", "Liechtenstein", "Lithuania", "Luxembourg", "Malta",
+  "Moldova", "Monaco", "Montenegro", "Netherlands", "Macedonia", "Norway",
+  "Poland", "Portugal", "Romania", "San Marino", "Serbia", "Slovakia",
+  "Slovenia", "Spain", "Sweden", "Switzerland", "Ukraine", "United Kingdom",
+  "Vatican"
 ];
 
 const africanCountries = [
-  "Algeria",
-  "Angola",
-  "Benin",
-  "Botswana",
-  "Burkina Faso",
-  "Burundi",
-  "Cabo Verde",
-  "Cameroon",
-  "Central African Republic",
-  "Chad",
-  "Comoros",
-  "Congo",
-  "Democratic Republic of the Congo",
-  "Djibouti",
-  "Egypt",
-  "Equatorial Guinea",
-  "Eritrea",
-  "Eswatini",
-  "Ethiopia",
-  "Gabon",
-  "Gambia",
-  "Ghana",
-  "Guinea",
-  "Guinea-Bissau",
-  "Ivory Coast",
-  "Kenya",
-  "Lesotho",
-  "Liberia",
-  "Libya",
-  "Madagascar",
-  "Malawi",
-  "Mali",
-  "Mauritania",
-  "Mauritius",
-  "Morocco",
-  "Mozambique",
-  "Namibia",
-  "Niger",
-  "Nigeria",
-  "Rwanda",
-  "Sao Tome and Principe",
-  "Senegal",
-  "Seychelles",
-  "Sierra Leone",
-  "Somalia",
-  "South Africa",
-  "South Sudan",
-  "Sudan",
-  "Tanzania",
-  "Togo",
-  "Tunisia",
-  "Uganda",
-  "Zambia",
-  "Zimbabwe",
+  "Algeria", "Angola", "Benin", "Botswana", "Burkina Faso", "Burundi",
+  "Cameroon", "Cape Verde", "Central African Rep.", "Chad", "Comoros",
+  "Congo", "Côte d'Ivoire", "Dem. Rep. Congo", "Djibouti", "Egypt",
+  "Eq. Guinea", "Eritrea", "eSwatini", "Ethiopia", "Gabon", "Gambia",
+  "Ghana", "Guinea", "Guinea-Bissau", "Kenya", "Lesotho", "Liberia",
+  "Libya", "Madagascar", "Malawi", "Mali", "Mauritania", "Mauritius",
+  "Morocco", "Mozambique", "Namibia", "Niger", "Nigeria", "Rwanda",
+  "Sao Tome and Principe", "Senegal", "Seychelles", "Sierra Leone",
+  "Somalia", "South Africa", "S. Sudan", "Sudan", "Tanzania", "Togo",
+  "Tunisia", "Uganda", "Zambia", "Zimbabwe"
 ];
 
 const antarctica = ["Antarctica"];
 
 const asiaTemperateCountries = [
-  "China",
-  "Japan",
-  "Mongolia",
-  "North Korea",
-  "South Korea",
-  "Taiwan",
+  "China", "Japan", "Mongolia", "North Korea", "South Korea", "Taiwan"
 ];
 
 const asiaTropicalCountries = [
-  "Bangladesh",
-  "Bhutan",
-  "Brunei",
-  "Cambodia",
-  "India",
-  "Indonesia",
-  "Laos",
-  "Malaysia",
-  "Maldives",
-  "Myanmar",
-  "Nepal",
-  "Pakistan",
-  "Philippines",
-  "Singapore",
-  "Sri Lanka",
-  "Thailand",
-  "Timor-Leste",
-  "Vietnam",
+  "Bangladesh", "Bhutan", "Brunei", "Cambodia", "India", "Indonesia", "Laos",
+  "Malaysia", "Maldives", "Myanmar", "Nepal", "Pakistan", "Philippines",
+  "Singapore", "Sri Lanka", "Thailand", "Timor-Leste", "Vietnam"
 ];
 
 const australasianCountries = [
-  "Australia",
-  "New Zealand",
-  "Papua New Guinea",
-  "Fiji",
-  "Solomon Islands",
-  "Vanuatu",
-  "Samoa",
-  "Tonga",
-  "Kiribati",
-  "Micronesia",
-  "Palau",
-  "Marshall Islands",
-  "Nauru",
-  "Tuvalu",
+  "Australia", "New Zealand", "Papua New Guinea", "Fiji", "Solomon Is.",
+  "Vanuatu", "Samoa", "Tonga", "Kiribati", "Micronesia", "Palau",
+  "Marshall Islands", "Nauru", "Tuvalu"
 ];
 
 const northAmericanCountries = [
-  "Canada",
-  "United States",
-  "Mexico",
-  "Guatemala",
-  "Belize",
-  "Honduras",
-  "El Salvador",
-  "Nicaragua",
-  "Costa Rica",
-  "Panama",
-  "Bahamas",
-  "Cuba",
-  "Jamaica",
-  "Haiti",
-  "Dominican Republic",
-  "Barbados",
-  "Saint Lucia",
-  "Grenada",
-  "Saint Vincent and the Grenadines",
-  "Saint Kitts and Nevis",
-  "Antigua and Barbuda",
-  "Dominica",
-  "Trinidad and Tobago",
+  "Canada", "United States of America", "Mexico", "Guatemala", "Belize",
+  "Honduras", "El Salvador", "Nicaragua", "Costa Rica", "Panama", "Bahamas",
+  "Cuba", "Jamaica", "Haiti", "Dominican Rep.", "Barbados", "Saint Lucia",
+  "Grenada", "Saint Vincent and the Grenadines", "Saint Kitts and Nevis",
+  "Antigua and Barbuda", "Dominica", "Trinidad and Tobago"
 ];
 
 const southAmericanCountries = [
-  "Argentina",
-  "Bolivia",
-  "Brazil",
-  "Chile",
-  "Colombia",
-  "Ecuador",
-  "Guyana",
-  "Paraguay",
-  "Peru",
-  "Suriname",
-  "Uruguay",
-  "Venezuela",
+  "Argentina", "Bolivia", "Brazil", "Chile", "Colombia", "Ecuador", "Guyana",
+  "Paraguay", "Peru", "Suriname", "Uruguay", "Venezuela"
 ];
 
 const pacificIslands = [
-  "Fiji",
-  "Kiribati",
-  "Marshall Islands",
-  "Micronesia",
-  "Nauru",
-  "Palau",
-  "Papua New Guinea",
-  "Samoa",
-  "Solomon Islands",
-  "Tonga",
-  "Tuvalu",
-  "Vanuatu",
+  "Fiji", "Kiribati", "Marshall Islands", "Micronesia", "Nauru", "Palau",
+  "Papua New Guinea", "Samoa", "Solomon Is.", "Tonga", "Tuvalu", "Vanuatu"
 ];
 
 const worldMarine = [
-  "Arctic Ocean",
-  "Indian Ocean",
-  "Southern Ocean",
-  "North Atlantic: unknown",
-  "North Atlantic: deep sea",
-  "North Atlantic: shelf area & adjacent sea",
-  "South Atlantic: unknown",
-  "South Atlantic: deep sea",
-  "South Atlantic: shelf area & adjacent seas",
-  "North Pacific: unknown",
-  "North Pacific: deep sea",
-  "North Pacific: shelf area & adjacent seas",
-  "South Pacific: unknown",
-  "South Pacific: deep sea",
-  "South Pacific: shelf area & adjacent seas",
+  "Arctic Ocean", "Indian Ocean", "Southern Ocean", "North Atlantic: unknown",
+  "North Atlantic: deep sea", "North Atlantic: shelf area & adjacent sea",
+  "South Atlantic: unknown", "South Atlantic: deep sea",
+  "South Atlantic: shelf area & adjacent seas", "North Pacific: unknown",
+  "North Pacific: deep sea", "North Pacific: shelf area & adjacent seas",
+  "South Pacific: unknown", "South Pacific: deep sea",
+  "South Pacific: shelf area & adjacent seas"
 ];
 
 const worldUnknown = ["World/NA"];
@@ -242,13 +84,13 @@ const allRegions = {
   Europe: europeanCountries,
   Africa: africanCountries,
   Antarctica: antarctica,
-  AsiaTemperate: asiaTemperateCountries,
-  AsiaTropical: asiaTropicalCountries,
+  "Asia Temperate": asiaTemperateCountries,
+  "Asia Tropical": asiaTropicalCountries,
   Australasia: australasianCountries,
-  NorthAmerica: northAmericanCountries,
-  SouthAmerica: southAmericanCountries,
-  PacificIslands: pacificIslands,
-  WorldMarine: worldMarine,
+  "North America": northAmericanCountries,
+  "South America": southAmericanCountries,
+  "Pacific Islands": pacificIslands,
+  "World Marine": worldMarine,
   WorldUnknown: worldUnknown,
 };
 
@@ -264,6 +106,7 @@ interface Props {
 const WorldMap = (props: Props) => {
   const { region } = props;
   let selectedRegion: string[] = [];
+
   if (region) {
     for (const r of region) {
       if (r in allRegions) {
@@ -276,10 +119,10 @@ const WorldMap = (props: Props) => {
       <ComposableMap
         projection="geoMercator"
         projectionConfig={{
-          scale: 120,
+          scale: 70,
         }}
-        width={800}
-        height={300}
+        width={900}
+        height={400}
       >
         <Geographies geography={geoUrl}>
           {({ geographies }) =>
