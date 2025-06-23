5599893f50a3cdca05c70da493c8dfa1df5a78b0
Aglorios17 <alessio.glorioso17@gmail.com>
2025-06-23 11:54:55 +0200

    Refactor SearchResult component to use Link for navigation and streamline click handling

--- src/components/search/components/SearchResult.tsx
+++ src/components/search/components/SearchResult.tsx
@@ -1,5 +1,5 @@
 /* Import Dependencies */
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import classNames from 'classnames';
 import { format } from 'date-fns';
 import { Row, Col } from 'react-bootstrap';
@@ -42,20 +42,24 @@ const SearchResult = (props: Props) => {
 
     if (taxonomicService)
     {
+        const id = taxonomicService.taxonomicService['@id'].replace(import.meta.env.VITE_HANDLE_URL as string, '');
+        const url = `/ts/${id}`;
+
+        const handleClick = (
+        e: React.MouseEvent<HTMLAnchorElement>,
+        data: TaxonomicService
+        ) => {
+            if (
+                e.ctrlKey || e.metaKey || e.shiftKey || e.button !== 0
+            ) return; // laisses le comportement natif (nouvel onglet, etc.)
+
+            e.preventDefault();
+            dispatch(setTaxonomicService(data));
+            navigate(url);
+        };
+
         /* Base variables */
         const logoImage: string | undefined = taxonomicService?.taxonomicService['schema:service']['schema:logo'];
-
-        /**
-         * Function for selecting a taxonomic service
-         * @param taxonomicService The selected taxonomic service
-         */
-        const SelectTaxonomicService = (taxonomicService: TaxonomicService) => {
-            /* Dispatch taxonomic service to store */
-            dispatch(setTaxonomicService(taxonomicService));
-
-            /* Navigate to the taxonomic service page */
-            navigate(`/ts/${taxonomicService.taxonomicService['@id'].replace(import.meta.env.VITE_HANDLE_URL as string, '')}`);
-        }
 
         /* ClassNames */
         const imageColClass = classNames({
@@ -68,9 +72,9 @@ const SearchResult = (props: Props) => {
 
         return (
             <div className={`${styles.searchResult} w-100 bgc-white mt-lg-1 pt-3 pb-2 px-3`}>
-                <button type="button"
-                    className="button-no-style"
-                    onClick={() => SelectTaxonomicService(taxonomicService)}
+                <Link
+                    to={url}
+                    onClick={(e) => handleClick(e, taxonomicService)}
                 >
                     <Row className="h-100">
                         {/* Basic column with all the details */}
@@ -147,25 +151,28 @@ const SearchResult = (props: Props) => {
                             </Row>
                         </Col>
                     </Row>
-                </button>
+                </Link>
             </div >
         );
     }
     else if (taxonomicExpert) {
         /* Base variables */
         const logoImage = taxonomicExpert?.taxonomicExpert?.['schema:person']?.['schema:ProfilePicture'] as string || 'https://i.pinimg.com/236x/d9/d8/8e/d9d88e3d1f74e2b8ced3df051cecb81d.jpg';
+        const id = taxonomicExpert.taxonomicExpert['@id'].replace(import.meta.env.VITE_HANDLE_URL as string, '');
+        const url = `/te/${id}`;
 
-        /**
-         * Function for selecting a taxonomic Expert
-         * @param taxonomicExpert The selected taxonomic Expert
-         */
-        const SelectTaxonomicExpert = (taxonomicExpert: TaxonomicExpert) => {
-            /* Dispatch taxonomic expert to store */
-            dispatch(setTaxonomicExpert(taxonomicExpert));
+        const handleClick = (
+        e: React.MouseEvent<HTMLAnchorElement>,
+        data: TaxonomicExpert
+        ) => {
+            if (
+                e.ctrlKey || e.metaKey || e.shiftKey || e.button !== 0
+            ) return; // laisses le comportement natif (nouvel onglet, etc.)
 
-            /* Navigate to the taxonomic expert page */
-            navigate(`/te/${taxonomicExpert.taxonomicExpert['@id'].replace(import.meta.env.VITE_HANDLE_URL as string, '')}`);
-        }
+            e.preventDefault();
+            dispatch(setTaxonomicExpert(data));
+            navigate(url);
+        };
 
         const name = taxonomicExpert?.taxonomicExpert?.['schema:person']?.['schema:name'] ?? '';
         const headline = taxonomicExpert?.taxonomicExpert?.['schema:person']?.['schema:headline'] ?? '';
@@ -175,9 +182,9 @@ const SearchResult = (props: Props) => {
         const subDiscipline = taxonomicExpert?.taxonomicExpert?.['schema:Taxon']?.['schema:additionalType'] || [];
         return (
             <div className={`${styles.searchResult} w-100 bgc-white mt-lg-1 pt-3 pb-2 px-3`}>
-                <button type="button"
-                    className="button-no-style"
-                    onClick={() => SelectTaxonomicExpert(taxonomicExpert)}
+               <Link
+                    to={url}
+                    onClick={(e) => handleClick(e, taxonomicExpert)}
                 >
                     <Col className="h-100 d-flex flex-column">
                         {/* Title, Headline */}
@@ -232,7 +239,7 @@ const SearchResult = (props: Props) => {
                             </Col>
                         </Row>
                     </Col>
-                </button>
+                </Link>
             </div >
             
         );
