2609f298458aa35b16d2965c29bc59a275f59dbd
Aglorios17 <alessio.glorioso17@gmail.com>
2025-07-22 12:13:13 +0200

    Update Support component to allow overflow in the container for better content visibility

--- src/components/support/Support.tsx
+++ src/components/support/Support.tsx
@@ -55,7 +55,7 @@ const Support = () => {
     return (
         <div className="h-100 d-flex flex-column">
             <Header />
-            <Container fluid className="flex-grow-1 overflow-hidden">
+            <Container fluid className="flex-grow-1 overflow-auto">
                 <Row className="h-100">
                     <Col lg={{ span: 10, offset: 1 }} className="h-100 d-flex flex-column pt-3 pt-lg-5 px-4 px-lg-3">
                         {(fetch.loading && displaySpinner) &&
